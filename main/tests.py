4a0866c95618bf2b5c6d7c3af732764d2164fe40
Sean Bolton <sean@smbolton.com>
2016-11-19 15:52:40 -0800

    add failing tests for __str__ for all models

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Song, Instance, Title, Collection
+
+
+class SongModelTest(TestCase):
+    def test_string_representation(self):
+        song = Song(digest='0' * 40)
+        song.save()
+        self.assertEqual(str(song), 'Song ' + str(song.id) + ' (' + song.digest[:7] + ')')
+
+
+class InstanceModelTest(TestCase):
+    def test_string_representation(self):
+        song = Song(digest='1' * 40)
+        song.save()
+        instance = Instance(song=song, digest='2' * 40,
+                            text='M:4/4\nL:1/4\nK:G\nF/G/Afe/d/|fe/d/eE|F/G/Afe/d/|d/G/F/E/FD:|')
+        instance.save()
+        self.assertEqual(str(instance), 'Instance ' + str(instance.id) + ' (' + instance.digest[:7] + ')')
+
+
+class TitleModelTest(TestCase):
+    def test_string_representation(self):
+        title = Title(title='Cast a Bell')
+        self.assertEqual(str(title), title.title)
+
+
+class CollectionModelTest(TestCase):
+    def test_string_representation(self):
+        collection = Collection(URL='http://www.smbolton.com/abc.html')
+        self.assertEqual(str(collection), collection.URL)
