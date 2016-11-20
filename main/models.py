74a1c339f330f14ec0880de8a6208c1ca8cfdc06
Sean Bolton <sean@smbolton.com>
2016-11-19 16:02:59 -0800

    add model __str__ methods

--- main/models.py
+++ main/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Song(models.Model):
     digest = models.CharField(max_length=40, unique=True, db_index=True)
 
+    def __str__(self):
+        return 'Song ' + str(self.id) + ' (' + self.digest[:7] + ')'
+
 
 class Instance(models.Model):
     # CREATE TABLE song_instance (song_id INTEGER, instance_id INTEGER);
@@ -12,12 +15,19 @@ class Instance(models.Model):
     digest = models.CharField(max_length=40, unique=True, db_index=True)
     text = models.TextField()
 
+    def __str__(self):
+        return 'Instance ' + str(self.id) + ' (' + self.digest[:7] + ')'
+
 
 class Title(models.Model):
     # CREATE TABLE title_song (title_id INTEGER, song_id INTEGER);
     # CREATE UNIQUE INDEX title_song_index ON title_song (title_id, song_id);
     songs = models.ManyToManyField(Song)
     title = models.CharField(max_length=80, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
 
 class Collection(models.Model):
     # CREATE TABLE collection_instance (collection_id INTEGER, instance_id INTEGER, X INTEGER);
@@ -26,3 +36,6 @@ class Collection(models.Model):
     # CREATE UNIQUE INDEX collection_instance_index ON collection_instance (collection_id, instance_id);
     instance = models.ManyToManyField(Instance)
     URL = models.URLField(unique=True)  # 200 char limit
+
+    def __str__(self):
+        return self.URL
