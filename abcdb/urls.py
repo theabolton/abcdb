27cfb9fce422a01f30288ff2e4c2f3bb093e0037
Sean Bolton <sean@smbolton.com>
2017-01-05 15:09:10 -0800

    add views of all models

--- abcdb/urls.py
+++ abcdb/urls.py
@@ -21,7 +21,16 @@ from main  import views as main_views
 
 
 urlpatterns = [
+    # project URLs
     url(r'^$', proj_views.RootView.as_view(), name='root'),
     url(r'^admin/', admin.site.urls),
+    # app URLs
+    url(r'^collection/(?P<pk>[0-9]{1,9})/$', main_views.CollectionView.as_view()),
+    url(r'^instance/(?P<pk>[0-9]{1,9})/$', main_views.InstanceView.as_view()),
     url(r'^upload/$', main_views.upload, name='upload'),
+    # temporary
+    url(r'^temp_songs/$', main_views.SongsView.as_view()),
+    url(r'^temp_instances/$', main_views.InstancesView.as_view()),
+    url(r'^temp_titles/$', main_views.TitlesView.as_view()),
+    url(r'^temp_collections/$', main_views.CollectionsView.as_view()),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import generic
 
 from main.forms import UploadForm
 
+
+# ========== User-oriented Model Views ==========
+
+class CollectionView(generic.DetailView):
+    model = Collection
+    template_name = 'main/collection.html'
+
+
+class InstanceView(generic.DetailView):
+    model = Instance
+    template_name = 'main/instance.html'
+
+
+# ========== ABC File Upload View ==========
 
 def upload(request):
     form_class = UploadForm
@@ -20,3 +35,37 @@ def upload(request):
     return render(request, 'main/upload.html', {
         'form': form_class,
     })
+
+
+# ========== Temporary Views for Development ==========
+
+class CollectionsView(generic.ListView):
+    template_name = 'main/temp_collections.html'
+    context_object_name = 'collection_list'
+
+    def get_queryset(self):
+        return Collection.objects.all().order_by('URL')
+
+
+class InstancesView(generic.ListView):
+    template_name = 'main/temp_instances.html'
+    context_object_name = 'instance_list'
+
+    def get_queryset(self):
+        return Instance.objects.all().order_by('digest')
+
+
+class SongsView(generic.ListView):
+    template_name = 'main/temp_songs.html'
+    context_object_name = 'song_list'
+
+    def get_queryset(self):
+        return Song.objects.all().order_by('digest')
+
+
+class TitlesView(generic.ListView):
+    template_name = 'main/temp_titles.html'
+    context_object_name = 'title_list'
+
+    def get_queryset(self):
+        return Title.objects.all().order_by('title')
